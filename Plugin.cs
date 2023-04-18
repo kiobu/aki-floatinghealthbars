c97d2f34ff125aaae3384fb123189c043b883af9
kiobu <kiobu@sdf.org>
2023-04-18 15:50:59 -0400

    feat: initial commit

---
+++ Plugin.cs
@@ -0,0 +1,151 @@
+﻿#pragma warning disable 0612
+#pragma warning disable 0618
+#pragma warning disable 0619
+
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.UI;
+using Comfort.Common;
+using EFT;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using TMPro;
+
+using Object = UnityEngine.Object;
+
+namespace FloatingHealthbars
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class FHPlugin : BaseUnityPlugin
+    {
+        private static GameObject _hookObject;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            _hookObject = new GameObject("HealthbarCore");
+            DontDestroyOnLoad(_hookObject);
+
+            new ExecuteOnRaidStartPatch().Enable();
+        }
+
+        public static void Execute(Player _player)
+        {
+            _hookObject.AddComponent<HealthbarCore>();
+        }
+    }
+
+    public class HealthbarCore : MonoBehaviour
+    {
+        public Object healthbarUIPrefab;
+        private static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("HealthbarCore");
+
+        public void Start()
+        {
+            logger.LogInfo("HealthbarCore began execution.");
+
+            // Handle bot spawn Action.
+            var botGame = Singleton<AbstractGame>.Instance as IBotGame;
+            botGame.BotsController.BotSpawner.OnBotCreated += HandleNewSpawn;
+
+            // Load healthbar assets.
+            string filepath = BepInEx.Paths.PluginPath + "/fhbars/" + "floatinghealthbars";
+            var bundle = AssetBundle.LoadFromFile(filepath);
+            healthbarUIPrefab = bundle.LoadAsset("Assets/Mypresets/Canvas.prefab");
+        }
+
+        private void HandleNewSpawn(BotOwner owner)
+        {
+            logger.LogInfo("New spawn.");
+            var healthbar = Instantiate(healthbarUIPrefab) as GameObject;
+            var tracker = healthbar.AddComponent<HealthbarTracker>();
+            tracker.owner = owner;
+            tracker.healthbar = healthbar;
+        }
+    }
+
+    public class HealthbarTracker : MonoBehaviour
+    {
+        private static Player LocalPlayerSingleton() => Singleton<GameWorld>.Instance.RegisteredPlayers.Find(p => p.IsYourPlayer);
+        private static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("HealthbarTracker");
+
+        public float offset = 2.0f;
+        private float maxHealth = 0.0f;
+
+        public BotOwner owner;
+        public GameObject healthbar;
+        public Canvas healthbarCanvas;
+
+        private Image healthbarImage;
+        private TextMeshPro textComponent;
+
+        private static float NormalizeHealth(float min, float max, float curr) => (curr - min) / (max - min) * 1;
+
+        public void Start()
+        {
+            logger.LogInfo("New HealthbarTracker instantiated, a bot should've been spawned.");
+            maxHealth = MaxHealth();
+
+            healthbarCanvas = healthbar.GetComponent<Canvas>();
+            healthbarImage = healthbarCanvas.GetComponentsInChildren<Image>().Single(c => c.name == "Healthbar");
+            textComponent = healthbarCanvas.GetComponentInChildren<TextMeshPro>();
+        }
+
+        public void Update()
+        {
+            // Not in world (impossible... probably).
+            if (LocalPlayerSingleton() == null)
+            {
+                logger.LogInfo("Not in world.");
+                return;
+            }
+
+            // Object destroyed (this is the parent object so I don't think this can be called).
+            if (healthbar == null)
+            {
+                logger.LogInfo("Healthbar was either destroyed or is null.");
+                return;
+            }
+
+            // Update healthbar info.
+            healthbar.transform.LookAt(LocalPlayerSingleton().Transform.position);
+            healthbar.transform.position = new Vector3(this.owner.Transform.position.x, this.owner.Transform.position.y + offset, this.owner.Transform.position.z);
+            healthbarImage.fillAmount = NormalizeHealth(0, maxHealth, CurrentHealth());
+            // textComponent.text = Convert.ToInt32(CurrentHealth()).ToString();
+            
+            // Bot died.
+            if (CurrentHealth() < 1 || owner.IsDead)
+            {
+                logger.LogInfo("Destroying healthbar for dead bot: " + owner.name);
+                healthbar.SetActive(false);
+                Destroy(healthbar);
+            }
+        }
+
+        private float CurrentHealth()
+        {
+            float health = 0;
+
+            foreach (EBodyPart bodyPart in Enum.GetValues(typeof(EBodyPart)))
+            {
+                health += owner.HealthController.GetBodyPartHealth(bodyPart).Current;
+            }
+
+            return health;
+        }
+
+        private float MaxHealth()
+        {
+            float health = 0;
+
+            foreach (EBodyPart bodyPart in Enum.GetValues(typeof(EBodyPart)))
+            {
+                health += owner.HealthController.GetBodyPartHealth(bodyPart).Maximum;
+            }
+
+            return health;
+        }
+    }
+}
